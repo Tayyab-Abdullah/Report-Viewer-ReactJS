76324f933767cbcd6891b408fd201d1ead0acd53
Tayyab Abdullah <tabdullah0801@gmail.com>
2023-07-28 15:23:37 +0500

    Added required images and json files having information for user and reports

---
+++ src/components/reportsData.js
@@ -0,0 +1,14 @@
+const reportsInfo = {
+  title: "CBC",
+  id: 1,
+  tlcValue: 1.5,
+  rbcValue: 1.6,
+  haemoglobinValue: 1.4,
+  pcvValue: 1.8,
+  mcvValue: 1.7,
+  mchValue: 1.3,
+  mchcValue: 1.2,
+  plateletValue: 1.3,
+};
+
+export default reportsInfo;
---
+++ src/components/userData.js
@@ -0,0 +1,9 @@
+const userInfo = {
+  name: "Tayyab Abdullah",
+  type: "Patient",
+  timeEnrolled: "09:00 AM",
+  opdRoom: 9112,
+  discountedBy: "Dr. Adam J.",
+};
+
+export default userInfo;
