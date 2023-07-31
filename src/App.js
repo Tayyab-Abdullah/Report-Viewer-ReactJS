3ec12790becfab45ffe1a2693f2353afb524607f
Tayyab Abdullah <tabdullah0801@gmail.com>
2023-07-31 14:19:05 +0500

    Added the navigation bar component and integrated it with the reports viewer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
-import './App.css';
-import LabReports from './components/labReports';
-
+import "./App.css";
+import LabReports from "./components/labReports";
+import NavigationBar from "./components/NavigationBar.js";
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />;
       <LabReports />
     </div>
   );
